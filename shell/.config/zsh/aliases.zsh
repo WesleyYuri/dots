8caf7df37ab01a09ca580d52e96985b8434eec2b
Wesley Yuri <wesleyyuridev@gmail.com>
2026-03-16 23:22:01 -0300

    feat: add dev alias and dotfiles alias

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,2 +1,30 @@
 [ -e /bin/lsd ] && alias ls="lsd"
 [ -e /bin/nvim ] && alias vim="nvim"
+
+# ############################
+# ### software-development ###
+# ############################
+export DEV_DIR="$HOME/software-development"
+
+if [[ -d "$DEV_DIR" ]]; then
+    alias dev="cd $DEV_DIR"
+
+    pdev() {
+        local directory
+        directory=$(fd -d 1 . "$DEV_DIR" | fzf) || return
+        [ -n "$directory" ] && cd "$directory" && nvim "$directory"
+    }
+else
+    mkdir $DEV_DIR
+fi
+
+# ################
+# ### dotfiles ###
+# ####+###########
+export DOTFILES_DIR="$HOME/.dotfiles"
+
+if [[ -d "$DOTFILES_DIR" ]]; then
+    alias dotconfig="nvim $DOTFILES_DIR"
+else
+    mkdir $DOTFILES_DIR
+fi
